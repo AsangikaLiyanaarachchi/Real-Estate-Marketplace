0b90ab3b14b5cb9ee1edd65b1cbba19ac8e2728c
Asangika <asangikanethmini@gmail.com>
2025-02-28 14:11:58 +0530

    Create header section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 //import React from 'react'
 
-export const Home = () => {
+const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>home</h1>
+      <h2>home</h2>
+    </div>
   )
 }
+
+export default Home
